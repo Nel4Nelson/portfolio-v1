e52bccc39cd1f10eb4b9e5587e359021f3d89966
Ekwueme Nelson <an.ekwueme@stu.unizik.edu.ng>
2023-01-20 14:35:48 +0000

    added routing

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <a href="#">Toby</a>
+
+                <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="skills">Skills</NuxtLink></li>
+                    <li><NuxtLink to="qualifications">Qualifications</NuxtLink></li>
+                    <li><NuxtLink to="projects">Projects</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+
+        <slot />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
