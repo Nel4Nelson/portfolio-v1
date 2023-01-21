d1014d3dd2b3e9835b8cf7ca80ffbf7754b45ffd
Ekwueme Nelson <an.ekwueme@stu.unizik.edu.ng>
2023-01-21 14:26:04 +0000

    implemented dark mode/home page and added assets

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,58 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <a href="#">Toby</a>
+        <header class="header">
+            <nav class="nav container">
+                <a href="#" class="nav__logo">Toby</a>
 
-                <ul>
-                    <li><NuxtLink to="/">Home</NuxtLink></li>
-                    <li><NuxtLink to="skills">Skills</NuxtLink></li>
-                    <li><NuxtLink to="qualifications">Qualifications</NuxtLink></li>
-                    <li><NuxtLink to="projects">Projects</NuxtLink></li>
-                </ul>
+                <div class="nav__menu" :class="{show: menu}">
+                    <ul class="nav__list grid">
+                        <li class="nav__item" @click="showMenu">
+                            <NuxtLink to="/" class="nav__link">
+                                <i class="ri-home-5-line"></i>
+                                Home
+                            </NuxtLink>
+                        </li>
+
+                        <li class="nav__item" @click="showMenu">
+                            <NuxtLink to="skills" class="nav__link">
+                                <i class="ri-trophy-line"></i>
+                                Skills
+                            </NuxtLink>
+                        </li>
+
+                        <li class="nav__item" @click="showMenu">
+                            <NuxtLink to="qualifications" class="nav__link">
+                                <i class="ri-book-open-line"></i>
+                                Qualifications
+                            </NuxtLink>
+                        </li>
+
+                        <li class="nav__item" @click="showMenu">
+                            <NuxtLink to="projects" class="nav__link">
+                                <i class="ri-image-line"></i>
+                                Projects
+                            </NuxtLink>
+                        </li>
+                    
+                    </ul>
+
+                    <!-- Close button -->
+                    <div class="nav__close" @click="showMenu">
+                     <i class="ri-close-line"></i>
+                    </div>
+                </div>
+
+                <div class="nav__buttons">
+                    <!-- Theme change button -->
+                    <div class="change-theme" @click="toggleDark()">
+                        <i v-if="isDark" class="ri-moon-line" ></i>
+                        <i v-else class="ri-sun-line"></i>
+                    </div>
+                    <div class="nav__toggle" @click="showMenu">
+                        <i class="ri-menu-4-line"></i>
+                    </div>
+                </div>
+                
             </nav>
         </header>
 
@@ -18,9 +61,23 @@
 </template>
 
 <script setup>
+    import { ref } from 'vue'
+    import {useDark, useToggle} from '@vueuse/core'
+    import 'remixicon/fonts/remixicon.css'
 
+    
+    const isDark = useDark();
+    const toggleDark = useToggle(isDark);
+
+    const menu = ref(false);
+
+    const showMenu = () =>  menu.value = !menu.value;
+    
 </script>
 
-<style scoped>
+<style >
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&family=Roboto:wght@400;500&display=swap");
+
+/*=============== VARIABLES CSS ===============*/
 
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,111 @@
 <template>
     <div>
-        <h2>Home</h2>
+        <main class="main">
+
+<section class="home section">
+    <div class="home__container container grid section__border">
+        <div class="home__data grid">
+            <h1 class="home__title">
+                Hi, I'm Toby Chidi <br>
+                Fullstack Developer <br>
+                Based In Nigeria <br>
+            </h1>
+
+            <div class="home__blob grid">
+                <div class="home__perfil">
+                    <img src="../assets/img/RTD_5452.JPG" alt="Toby's Picture">
+                </div>
+
+                <!-- <img src="../assets/img/shape-wawes.svg" alt="" class="home__shape-wawes">
+                <img src="../assets/img/shape-circle.svg" alt="" class="home__shape-circle"> -->
+            </div>
+
+            <ul class="home__social">
+                <a href="#" target="_blank" class="home__social-link">
+                    <i class="ri-linkedin-box-line"></i>
+                </a>
+                <a href="#" target="_blank" class="home__social-link">
+                    <i class="ri-github-line"></i>
+                </a>
+                <a href="#" target="_blank" class="home__social-link">
+                    <i class="ri-twitter-line"></i>
+                </a>
+            </ul>
+        </div>
+
+        <!-- Home Info 1 -->
+        <div class="home__info">
+            <div>
+                <h3 class="home__info-title">
+                    BIOGRAPHY
+                </h3>
+
+                <p class="home__info-description">
+                    Hi, I'm Toby, Fullstack Developer. Passionate about designing beautiful and scalable products.
+                </p>
+            </div>
+
+            <div>
+                <h3 class="home__info-title">
+                    CONTACT
+                </h3>
+
+                <p class="home__info-description">
+                    Awka, Nigeria <br>
+                    tobyubaezuonu@gmail.com <br>
+                    +234 9064546266
+                </p>
+            </div>
+
+            <div>
+                <h3 class="home__info-title">
+                    SERVICES
+                </h3>
+
+                <p class="home__info-description">
+                    Website App Design <br>
+                    Mobile/Desktop App Development <br>
+                    Animation
+                </p>
+            </div>
+        </div>
+
+        <!-- Home Info 2 -->
+        <div class="home__info">
+            <div>
+                <h3 class="home__info-title">
+                    YEARS OF EXPERIENCE
+                </h3>
+
+                <p class="home__info-number">
+                    05+
+                </p>
+            </div>
+
+            <div>
+                <h3 class="home__info-title">
+                    COMPLETED PROJECTS
+                </h3>
+
+                <p class="home__info-number">
+                    10+
+                </p>
+            </div>
+
+            <div>
+                <h3 class="home__info-title">
+                    COMPANIES WORKED
+                </h3>
+
+                <p class="home__info-number">
+                    08+
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+</main>
     </div>
 </template>
 
@@ -9,5 +114,8 @@
 </script>
 
 <style scoped>
+   
+   
+
 
 </style>
